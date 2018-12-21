08c7f090ab7ea056cb5e5f3d6a0a0c9f644feb27
Sean Nealon <signalflowsean@gmail.com>
2018-12-21 15:57:46 -0500

    added description

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,6 +7,9 @@ class App extends Component {
     return (
       <div>
         <h1>PETFUL ADOPTION</h1>
+        <p>Adopt a pet by clicking the adopt button</p>
+        <p>Once the adopt button is clicked you will be able to see the next pet</p>
+        <p>Thanks for giving these adorable muffins a home</p> 
         <Dashboard></Dashboard>
       </div>
     );
