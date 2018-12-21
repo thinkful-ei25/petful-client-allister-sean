5e71e71785938e7510967317666c3533f3f9e044
Sean Nealon <signalflowsean@gmail.com>
2018-12-21 16:59:14 -0500

    changed styeles folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'; 
-import './index.css';
+import './styles/index.css';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
 import store from './store'; 
