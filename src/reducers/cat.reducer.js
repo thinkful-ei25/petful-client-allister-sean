1b5d392d3a5955747e219f8b35c1ec6f237e09f9
Sean Nealon <signalflowsean@gmail.com>
2018-12-21 13:51:07 -0500

    added reducers

--- src/reducers/cat.reducer.js
+++ src/reducers/cat.reducer.js
@@ -2,7 +2,6 @@ import {
     FETCH_CAT_LOADING, 
     FETCH_CAT_SUCCESS, 
     FETCH_CAT_ERROR, 
-    ADOPT_CAT_LOADING, 
     ADOPT_CAT_ERROR
 } from '../actions/index.action'; 
 
@@ -15,5 +14,29 @@ const initialState = {
 }
 
 export default function reducer(state=initialState, action){ 
+    if (action.type === FETCH_CAT_LOADING) { 
+        const newCat = Object.assign({}, state.cat, 
+            {loading : true, data: null, error: null}); 
+         
+        return Object.assign({}, state, { cat : newCat}); 
+    }
+    else if (action.type === FETCH_CAT_SUCCESS) { 
+        const newCat = Object.assign({}, state.cat, 
+            {loading: false, data: action.cat, error: null}); 
+        
+        return Object.assign({}, state, {cat : newCat}); 
+    }
+    else if (action.type === FETCH_CAT_ERROR) { 
+        const newCat = Object.assign({}, state.cat, 
+            {loading : false, data: null, error: action.err}); 
+        
+        return Object.assign({}, state.cat, {cat: newCat}); 
+    }
+    else if (action.type === ADOPT_CAT_ERROR) {
+        const newCat = Object.assign({}, state.car, 
+            {loading: false, data:null, error : action.err}); 
+
+        return Object.assign({}, state, {cat: newCat}); 
+    }
     return state; 
 }
--- src/reducers/dog.reducer.js
+++ src/reducers/dog.reducer.js
@@ -2,7 +2,6 @@ import {
     FETCH_DOG_LOADING, 
     FETCH_DOG_SUCCESS, 
     FETCH_DOG_ERROR,
-    ADOPT_DOG_LOADING, 
     ADOPT_DOG_ERROR  
 } from '../actions/index.action'; 
 
@@ -15,5 +14,29 @@ const initialState = {
 }
 
 export default function reducer(state=initialState, action) { 
+    if (action.type === FETCH_DOG_LOADING) { 
+        const newDog = Object.assign({}, state.dog, 
+            {loading:true, data:null, error:null}); 
+        
+        return Object.assign({}, state, {dog : newDog}); 
+    }
+    else if (action.type === FETCH_DOG_SUCCESS) { 
+        const newDog = Object.assign({}, state.dog, 
+            {loading:false, data: action.dog, error: null}); 
+        
+        return Object.assign({}, state, {dog: newDog}); 
+    }
+    else if (action.type === FETCH_DOG_ERROR) { 
+        const newDog = Object.assign({}, state.dog, 
+            {loading:false, data:null, error:action.err}); 
+        
+        return Object.assign({}, state, {dog: newDog}); 
+    }
+    else if (action.type === ADOPT_CAT_ERROR) { 
+        const newDog = Object.assign({}, state.dog, { 
+            loading: false, data: null, error: action.err}); 
+        
+        return Object.assign({}, state, {dog: newDog}); 
+    }
     return state; 
 }
