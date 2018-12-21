eb6bbb7aac0b1f2ddc4e2123afa3a9a20bbd82e9
Sean Nealon <signalflowsean@gmail.com>
2018-12-21 15:55:29 -0500

    added title and cleaned up state

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,34 +3,10 @@ import '../App.css';
 import Dashboard from './Dashboard';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { cat : { 
-      
-        imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-        imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-        name: 'Fluffy',
-        sex: 'Female',
-        age: 2,
-        breed: 'Bengal',
-        story: 'Thrown on the street'
-    },
-    dog : {
-      
-        imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-        imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-        name: 'Zeus',
-        sex: 'Male',
-        age: 3,
-        breed: 'Golden Retriever',
-        story: 'Owner Passed away'
-      }
-    }
-  }
-
   render() {
     return (
       <div>
+        <h1>PETFUL ADOPTION</h1>
         <Dashboard></Dashboard>
       </div>
     );
